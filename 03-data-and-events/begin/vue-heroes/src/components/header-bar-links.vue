8346dc9fee7b1ace936e486b60cfe7441e0686cf
Erick Vidbaz <support@vidbaz.net>
2021-02-12 00:19:31 +0100

    chapter 03 finished

--- 03-data-and-events/begin/vue-heroes/src/components/header-bar-links.vue
+++ 03-data-and-events/begin/vue-heroes/src/components/header-bar-links.vue
@@ -3,18 +3,10 @@
     <div class="navbar-end">
       <div class="navbar-item">
         <div class="buttons">
-          <a
-            :href="github"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a :href="github" target="_blank" rel="noopener noreferrer">
             <i class="fab fa-github fa-2x" aria-hidden="true"></i>
           </a>
-          <a
-            :href="twitter"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a :href="twitter" target="_blank" rel="noopener noreferrer">
             <i class="fab fa-twitter fa-2x" aria-hidden="true"></i>
           </a>
         </div>
@@ -24,10 +16,10 @@
 </template>
 <script>
 export default {
-  name: "HeaderBarLinks",
+  name: 'HeaderBarLinks',
   data() {
     return {
-      github: 'https://github.com/johnpapa/vue-getting-started',
+      github: 'https://github.com/vidbaz/vue-getting-started',
       twitter: 'https://twitter.com/vidbaz',
     };
   },
